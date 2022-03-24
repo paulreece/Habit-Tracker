0b0012532e64c1234c0da43aca9d26a886065593
Paul Reece <paulreece2@gmail.com>
2022-03-24 15:45:41 -0400

    big auto removed

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -140,7 +140,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "habitapp.User"
 ACCOUNT_ACTIVATION_DAYS = 100
 
